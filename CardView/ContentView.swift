94a2958004c7d2af838abdbe47b4a0a78a206d29
fakhareldin abakar <39832576+wadshatta@users.noreply.github.com>
2023-07-06 14:04:13 +0500

    final view

--- CardView/ContentView.swift
+++ CardView/ContentView.swift
@@ -8,19 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("swiftui-button")
+                       .resizable()
+                   .aspectRatio(contentMode: .fit)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text(author.uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                .layoutPriority(100)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(image: "swiftui-button", category: "Swwiftui", heading: "Drawing a Border with Rounded Corners", author: "fakhareldin")
     }
 }

5d92d2e588f4f4dbe4ce2524ed36207dd213b394
fakhareldin abakar <39832576+wadshatta@users.noreply.github.com>
2023-07-06 13:43:15 +0500

    I haven't opened the program for a while, so I thought I might forget something in this area, so I decided to review all this stuff

---
+++ CardView/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  CardView
+//
+//  Created by MacBook Pro on 05/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
